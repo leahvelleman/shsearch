ec9a7a6d347eaff287b77814a3647f71fd2013c6
Leah Velleman <leahvelleman@gmail.com>
2020-07-03 17:18:23 -0400

    Search form, but with garbage logic -- just the form and views themselves

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 from app import app
 from app.models import Song
+from .forms import SearchForm
 
 
 @app.route('/')
@@ -10,6 +11,22 @@ def index():
 
 
 @app.route('/songs')
-def search():
+def all_songs():
     songs = Song.query.all()
+    return render_template('results.html', songs=songs)
+
+
+@app.route('/search', methods=('GET', 'POST'))
+def search():
+    form = SearchForm(request.form)
+    if form.validate_on_submit():
+        return redirect(url_for(
+                            endpoint='search_results',
+                            query=form.search_string.data))
+    return render_template('search.html', form=form)
+
+
+@app.route('/search_results/<query>')
+def search_results(query):
+    songs = Song.query.filter_by(id = query).all()
     return render_template('results.html', songs=songs)
--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = 'snarfleblorf'
