7a7e6476e126d3b4e3d0a2f3066ce251f5c54c01
Leah Velleman <leahvelleman@gmail.com>
2020-07-03 15:14:26 -0400

    Basic search result template and all songs route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
+from flask import render_template
 from app import app
+from app.models import Song
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return app.config['SQLALCHEMY_DATABASE_URI']
+
+
+@app.route('/songs')
+def search():
+    songs = Song.query.all()
+    return render_template('results.html', songs=songs)
