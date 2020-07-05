f89881a740e3f8610c31c7b5a321705f8488514d
Leah Velleman <leahvelleman@gmail.com>
2020-07-05 16:21:32 -0400

    Refactor templates to make search form an include and consolidate routes

--- app/routes.py
+++ app/routes.py
@@ -5,35 +5,31 @@ from app.models import Song
 from .forms import SearchForm
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return app.config['SQLALCHEMY_DATABASE_URI']
-
-
 @app.route('/songs')
 def all_songs():
     songs = Song.query.all()
     return render_template('results.html', songs=songs)
 
 
-@app.route('/search', methods=('GET', 'POST'))
+@app.route('/', methods=('GET', 'POST'))
 def search():
     form = SearchForm(request.form)
     if form.validate_on_submit():
         return redirect(url_for(
                             endpoint='search_results',
                             query=form.search_string.data))
-    return render_template('search.html', form=form)
+    return render_template('index.html', form=form)
 
 
-@app.route('/search_results')
+@app.route('/search')
 def search_results():
+    form = SearchForm(request.form)
+
     filters = []
     for k in request.args:
         column = getattr(Song, k)
         comparison_string = '%{}%'.format(request.args[k])
         filters.append(column.like(comparison_string))
-
     songs = Song.query.filter(and_(*filters))
-    return render_template('results.html', songs=songs)
+
+    return render_template('search.html', songs=songs, form=form)
