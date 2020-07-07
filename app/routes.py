94e0353ecdc66e85f8e3c932de20d1000c1d3e21
Leah Velleman <leahvelleman@gmail.com>
2020-07-07 12:08:25 -0400

    Tests and features for very basic error handling. Update routes I wasn't using much.

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,40 @@
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, flash
 from sqlalchemy import and_
 from app import app
 from app.models import Song
 from .forms import SearchForm
 
 
-@app.route('/songs')
-def all_songs():
+@app.route('/songs/')
+def songs():
+    form = SearchForm(request.form)
     songs = Song.query.all()
-    return render_template('results.html', songs=songs)
+    return render_template('search.html', songs=songs, form=form)
 
 
 @app.route('/', methods=('GET', 'POST'))
-def search():
+@app.route('/index.html', methods=('GET', 'POST'))
+def index():
     form = SearchForm(request.form)
     if form.validate_on_submit():
         return redirect(url_for(
-                            endpoint='search_results',
+                            endpoint='search',
                             query=form.search_string.data))
     return render_template('index.html', form=form)
 
 
 @app.route('/search')
-def search_results():
+def search():
     form = SearchForm(request.form)
 
     filters = []
     for k in request.args:
-        column = getattr(Song, k)
-        comparison_string = '%{}%'.format(request.args[k])
-        filters.append(column.like(comparison_string))
+        try:
+            column = getattr(Song, k)
+            comparison_string = '%{}%'.format(request.args[k])
+            filters.append(column.like(comparison_string))
+        except AttributeError:
+            flash("Not a valid search term: {}".format(k))
     songs = Song.query.filter(and_(*filters))
 
     return render_template('search.html', songs=songs, form=form)
--- app/tests/test_foo.py
+++ app/tests/test_foo.py
@@ -1,3 +1,4 @@
+from flask import url_for
 from app.models import Song
 from app import app
 
@@ -6,6 +7,22 @@ def test_root_route_is_200():
     with app.test_client() as client:
         rv = client.get('/')
         assert rv.status_code == 200
+
+
+def test_search_key_errors_are_flashed():
+    with app.test_client() as client:
+        rv = client.get('/search?invalidkey=foo')
+        assert rv.status_code == 200
+        assert b'<ul class="AttributeError">' in rv.data
+        assert b'invalidkey' in rv.data
+
+
+def test_useful_404_page():
+    with app.test_client() as client:
+        rv = client.get('/invalidroutename')
+        assert rv.status_code == 404
+        assert rv.data
+        assert url_for('index') in str(rv.data)
 
 
 def test_song_get():
