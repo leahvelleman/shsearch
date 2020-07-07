acb3487baca51c96fb1c3eae3064e8ecc622deb5
Leah Velleman <leahvelleman@gmail.com>
2020-07-06 21:04:30 -0400

    Basic test of root route

--- app/tests/test_foo.py
+++ app/tests/test_foo.py
@@ -1,4 +1,11 @@
 from app.models import Song
+from app import app
+
+
+def test_root_route_is_200():
+    with app.test_client() as client:
+        rv = client.get('/')
+        assert rv.status_code == 200
 
 
 def test_song_get():
