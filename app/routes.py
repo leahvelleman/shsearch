01500607afd9daad4f90659b965810f6dc6e316c
Leah Velleman <leahvelleman@gmail.com>
2020-08-06 11:13:49 -0400

    Rudimentary verse formatter forked from whoosh's SentenceFormatter

--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,88 @@ from flask import render_template, redirect, request, url_for
 from app import app
 from .forms import SearchForm
 from .schema import schema, CREATOR, FULLTEXT
-from whoosh.highlight import Highlighter, WholeFragmenter, ContextFragmenter
+from whoosh.highlight import Highlighter, Fragmenter, WholeFragmenter, DEFAULT_CHARLIMIT, mkfrag, HtmlFormatter
 from whoosh.sorting import Count
 from whoosh.query import Term, Or, And, Every, Phrase
 from whoosh import index as _index
+
+
+class VerseFragmenter(Fragmenter):
+    """Breaks the text up on sentence end punctuation characters
+    (".", "!", or "?"). This object works by looking in the original text for a
+    sentence end as the next character after each token's 'endchar'.
+    When highlighting with this fragmenter, you should use an analyzer that
+    does NOT remove stop words, for example::
+        sa = StandardAnalyzer(stoplist=None)
+    """
+
+    def __init__(self, maxchars=200, sentencechars=".!?",
+                 charlimit=DEFAULT_CHARLIMIT):
+        """
+        :param maxchars: The maximum number of characters allowed in a
+            fragment.
+        """
+
+        self.maxchars = maxchars
+        self.sentencechars = frozenset(sentencechars)
+        self.charlimit = charlimit
+
+    def fragment_tokens(self, text, tokens):
+        maxchars = self.maxchars
+        sentencechars = self.sentencechars
+        charlimit = self.charlimit
+
+        textlen = len(text)
+        # startchar of first token in the current sentence
+        first = None
+        # Buffer for matched tokens in the current sentence
+        tks = []
+        endchar = None
+        # Number of chars in the current sentence
+        currentlen = 0
+
+        for t in tokens:
+            startchar = t.startchar
+            endchar = t.endchar
+            if charlimit and endchar > charlimit:
+                break
+
+            if first is None:
+                # Remember the startchar of the first token in a sentence
+                first = startchar
+                currentlen = 0
+
+            tlength = endchar - startchar
+            currentlen += tlength
+
+            if t.matched:
+                tks.append(t.copy())
+
+            # If the character after the current token is end-of-sentence
+            # punctuation, finish the sentence and reset
+            if endchar < textlen and text[endchar] in sentencechars and endchar + 1 < textlen and text[endchar + 1] == "\n":
+
+                # If the sentence had matches and it's not too long, yield it
+                # as a token
+                if tks and currentlen <= maxchars:
+                    yield mkfrag(text, tks, startchar=first, endchar=endchar)
+                # Reset the counts
+                tks = []
+                first = None
+                currentlen = 0
+
+        # If we get to the end of the text and there's still a sentence
+        # in the buffer, yield it
+        if tks:
+            yield mkfrag(text, tks, startchar=first, endchar=endchar)
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 indexdir = os.path.join(THIS_FOLDER, '../indexdir')
 
 ix = _index.open_dir(indexdir)
 
-cfh = Highlighter(fragmenter=ContextFragmenter())
+cfh = Highlighter(fragmenter=VerseFragmenter(sentencechars='.?!;:’”)'),
+        formatter=HtmlFormatter(between="\n...\n"))
 wfh = Highlighter(fragmenter=WholeFragmenter())
 
 
